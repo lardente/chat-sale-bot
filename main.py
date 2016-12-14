dc0927e2361f4077dafdcd1f868ceaac39a19a55
Luiz Ardente <gustavo.ardente@gmail.com>
2016-12-14 02:03:23 -0200

    Update main body.

--- main.py
+++ main.py
@@ -11,10 +11,7 @@ class ThingsResource(object):
     def on_get(self, req, resp):
         """Handles GET requests"""
         resp.status = falcon.HTTP_200  # This is the default status
-        resp.body = ('\nTwo things awe me most, the starry sky '
-                     'above me and the moral law within me.\n'
-                     '\n'
-                     '    ~ Immanuel Kant\n\n')
+        resp.body = ('1739505827')
 
 # falcon.API instances are callable WSGI apps
 app = falcon.API()
