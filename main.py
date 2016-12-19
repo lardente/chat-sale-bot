8b933806dcc8e020f16d587a95fa0d5f0fb1c7a0
Luiz Ardente <gustavo.ardente@gmail.com>
2016-12-19 00:54:45 -0200

    Testing handshake.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # things.py
 
 # Let's get this party started!
+import os
 import falcon
 
 # Falcon follows the REST architectural style, meaning (among
 # other things) that you think in terms of resources and state
 # transitions, which map to HTTP verbs.
+VERIFY_TOKEN = (os.environ['VERIFY_TOKEN'], 'oi_eu_sou_um_teste')
 
 
 class ThingsResource(object):
@@ -13,7 +15,16 @@ class ThingsResource(object):
     def on_get(self, req, resp):
         """Handles GET requests"""
         resp.status = falcon.HTTP_200  # This is the default status
-        resp.body = ('1739505827')
+        
+        if req.args.get('hub.verify_token', '') == VERIFY_TOKEN:
+            return req.args.get('hub.challenge', '')
+        else:
+            return 'Error, wrong validation token'
+
+    def on_post(self, req, resp):
+        req.get_data()
+        return "ok"
+
 
 # falcon.API instances are callable WSGI apps
 app = falcon.API()
